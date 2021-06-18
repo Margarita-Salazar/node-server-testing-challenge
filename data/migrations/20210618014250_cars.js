8d7d4b8641e2daf99057ad8270906cb1180b09f1
margarita-salazar <margarita.salazar.mls@gmail.com>
2021-06-17 20:49:20 -0500

    added a table for cars

---
+++ data/migrations/20210618014250_cars.js
@@ -0,0 +1,10 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', table => {
+        table.increments('car_id')
+        table.string('name').unique().notNullable()
+      });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("cars")
+};
