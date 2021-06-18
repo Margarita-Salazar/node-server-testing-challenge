2fab0607315a815afd2d0a288d3cc791a953c0d1
margarita-salazar <margarita.salazar.mls@gmail.com>
2021-06-18 00:37:04 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -4,4 +4,31 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/cars', (req, res) => {
+    res.end()
+})
+
+server.get('/api/cars/:id', (req, res) => {
+    res.end()
+})
+
+server.post('/api/cars', (req, res) => {
+    res.end()
+})
+
+server.put('/api/cars/:id', (req, res) => {
+    res.end()
+})
+
+server.delete('/api/cars/:id', (req, res) => {
+    res.end()
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    });
+})
+
 module.exports = server;
